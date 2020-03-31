d5b152bcf1edc9c72a73627dd1cd224d2e49b942
Rafał Przędzik <46092482+rprzedzik@users.noreply.github.com>
2020-03-31 10:29:31 +0200

    Extend magento 1 csv import (#444)

--- src/Application/Controller/Api/Option/OptionCreateAction.php
+++ src/Application/Controller/Api/Option/OptionCreateAction.php
@@ -7,18 +7,12 @@
 
 declare(strict_types = 1);
 
-namespace Ergonode\Attribute\Application\Controller\Api\Attribute;
+namespace Ergonode\Attribute\Application\Controller\Api\Option;
 
 use Ergonode\Api\Application\Exception\FormValidationHttpException;
-use Ergonode\Api\Application\Response\EmptyResponse;
-use Ergonode\Attribute\Application\Form\AttributeUpdateForm;
-use Ergonode\Attribute\Application\Form\Model\UpdateAttributeFormModel;
-use Ergonode\Attribute\Domain\Command\UpdateAttributeCommand;
-use Ergonode\Attribute\Domain\Entity\AbstractAttribute;
-use Ergonode\Attribute\Domain\ValueObject\AttributeType;
+use Ergonode\Api\Application\Response\CreatedResponse;
 use Ergonode\Core\Domain\ValueObject\TranslatableString;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Swagger\Annotations as SWG;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -26,56 +20,65 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\PropertyAccess\Exception\InvalidPropertyPathException;
 use Symfony\Component\Routing\Annotation\Route;
+use Ergonode\Attribute\Domain\Entity\AbstractAttribute;
+use Ergonode\Attribute\Application\Form\Model\Option\SimpleOptionModel;
+use Ergonode\Attribute\Application\Form\SimpleOptionForm;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Ergonode\Attribute\Domain\ValueObject\OptionKey;
+use Ergonode\Attribute\Domain\Command\Option\CreateOptionCommand;
 use Ergonode\EventSourcing\Infrastructure\Bus\CommandBusInterface;
 
 /**
  * @Route(
- *     name="ergonode_attribute_change",
- *     path="/attributes/{attribute}",
- *     methods={"PUT"},
- *     requirements={"attribute" = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"}
+ *     name="ergonode_option_create",
+ *     path="/attributes/{attribute}/options",
+ *     methods={"POST"},
+ *     requirements={
+ *        "attribute" = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
+ *     }
  * )
  */
-class AttributeChangeAction
+class OptionCreateAction
 {
-    /**
-     * @var CommandBusInterface
-     */
-    private CommandBusInterface $commandBus;
-
     /**
      * @var FormFactoryInterface
      */
     private FormFactoryInterface $formFactory;
 
     /**
-     * @param CommandBusInterface  $commandBus
-     * @param FormFactoryInterface $formFactory
+     * @var CommandBusInterface
      */
-    public function __construct(CommandBusInterface $commandBus, FormFactoryInterface $formFactory)
+    private CommandBusInterface $commandBus;
+
+    /**
+     * @param FormFactoryInterface $formFactory
+     * @param CommandBusInterface  $commandBus
+     */
+    public function __construct(FormFactoryInterface $formFactory, CommandBusInterface $commandBus)
     {
-        $this->commandBus = $commandBus;
         $this->formFactory = $formFactory;
+        $this->commandBus = $commandBus;
     }
 
     /**
-     * @IsGranted("ATTRIBUTE_UPDATE")
+     * @IsGranted("ATTRIBUTE_CREATE")
      *
      * @SWG\Tag(name="Attribute")
      * @SWG\Parameter(
-     *     name="attribute",
+     *     name="language",
      *     in="path",
      *     type="string",
-     *     description="Attribute id",
+     *     description="Language code",
+     *     default="EN"
      * )
      * @SWG\Parameter(
      *     name="body",
      *     in="body",
      *     description="Add attribute",
      *     required=true,
-     *     @SWG\Schema(ref="#/definitions/attribute")
+     *     @SWG\Schema(ref="#/definitions/option")
      * )
-     *  @SWG\Parameter(
+     * @SWG\Parameter(
      *     name="language",
      *     in="path",
      *     type="string",
@@ -84,48 +87,44 @@ class AttributeChangeAction
      *     description="Language Code",
      * )
      * @SWG\Response(
-     *     response=200,
-     *     description="Returns attribute",
+     *     response=201,
+     *     description="Returns attribute ID",
      * )
      * @SWG\Response(
      *     response=400,
      *     description="Validation error",
      *     @SWG\Schema(ref="#/definitions/validation_error_response")
      * )
-     * @SWG\Response(
-     *     response=404,
-     *     description="Not found",
-     * )
-     *
-     * @ParamConverter(class="Ergonode\Attribute\Domain\Entity\AbstractAttribute")
      *
      * @param AbstractAttribute $attribute
      * @param Request           $request
      *
+     * @ParamConverter(class="Ergonode\Attribute\Domain\Entity\AbstractAttribute")
+     *
      * @return Response
+     *
+     * @throws \Exception
      */
     public function __invoke(AbstractAttribute $attribute, Request $request): Response
     {
         try {
-            $model = new UpdateAttributeFormModel(new AttributeType($attribute->getType()));
-            $form = $this->formFactory->create(AttributeUpdateForm::class, $model, ['method' => Request::METHOD_PUT]);
+            $model = new SimpleOptionModel();
+            $form = $this->formFactory->create(SimpleOptionForm::class, $model);
             $form->handleRequest($request);
 
             if ($form->isSubmitted() && $form->isValid()) {
-                /** @var UpdateAttributeFormModel $data */
+                /** @var SimpleOptionModel $data */
                 $data = $form->getData();
 
-                $command = new UpdateAttributeCommand(
+                $command = new CreateOptionCommand(
                     $attribute->getId(),
-                    new TranslatableString($data->label),
-                    new TranslatableString($data->hint),
-                    new TranslatableString($data->placeholder),
-                    $data->groups,
-                    (array) $data->parameters
+                    new OptionKey($data->code),
+                    new TranslatableString($data->label)
                 );
+
                 $this->commandBus->dispatch($command);
 
-                return new EmptyResponse();
+                return new CreatedResponse($command->getId());
             }
         } catch (InvalidPropertyPathException $exception) {
             throw new BadRequestHttpException('Invalid JSON format');
